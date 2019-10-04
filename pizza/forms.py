681080c8dbbf42c99b2d592fda3cce2921cbf438
Amanu Haile <ahaile505@gmail.com>
2019-10-03 19:40:58 -0700

    Added mutiple pizza form and edit order

--- pizza/forms.py
+++ pizza/forms.py
@@ -13,20 +13,31 @@ from .models import Pizza, Size
 
 # class PizzaForm(forms.ModelForm):
 
-#     size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
+#     # size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
 
 #     class Meta:
 #         model = Pizza
 #         fields = ['topping1', 'topping2', 'size']
 #         labels = {'topping1': 'Topping 1','topping2': 'Topping 2', 'size': 'Size' }
-#         # widgets = {'size': forms.CheckboxSelectMultiple}
+#         widgets = {'size': forms.CheckboxSelectMultiple}
+
+# class PizzaForm(forms.ModelForm):
+
+#     image = forms.ImageField()
+
+#     class Meta:
+#         model = Pizza
+#         fields = ['topping1', 'topping2', 'size']
+#         labels = {'topping1': 'Topping 1','topping2': 'Topping 2', 'size': 'Size' }
+  
 
 class PizzaForm(forms.ModelForm):
-
-    image = forms.ImageField()
-
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
         labels = {'topping1': 'Topping 1','topping2': 'Topping 2', 'size': 'Size' }
+
+class MutiplePizzaForm(forms.Form):
+
+    number = forms.IntegerField(min_value=2, max_value=6)
   
